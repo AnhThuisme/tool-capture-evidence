dcdc15407a7229d1b83d8f9cd7cf7ab916658571
ginngobap-sketch <ginngocbap@gmail.com>
2026-03-16 18:17:19 +0700

    minimal api test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "running"}
-
-handler = Mangum(app)
+    return {"status": "ok"}
