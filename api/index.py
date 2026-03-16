70c35cdce3fdb3d475ee5f8b1092a99c06be0e19
ginngobap-sketch <ginngocbap@gmail.com>
2026-03-16 18:10:28 +0700

    fix vercel config

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "ok"}
+    return {"status": "running"}
+
+handler = Mangum(app)
