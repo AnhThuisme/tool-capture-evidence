56a39e80f00a049c3776c16cf31f2ca97189e0e8
AT <at@LAP250s-MacBook-Air.local>
2026-05-08 14:02:23 +0700

    Remove top-level Lauch Chrome action button from run panel

--- web_ui.py
+++ web_ui.py
@@ -3768,12 +3768,6 @@ linear-gradient(to right, transparent, transparent)}
                 </label>
                 <div id="bookingRunExtraToggles"></div>
                 <div class="run-actions-main">
-                  <button class="btn action-btn soft" type="button" onclick="loginFacebookBeforeRun()">
-                    <span class="action-icon" aria-hidden="true">
-                      <svg viewBox="0 0 24 24"><path d="M14 8h3V4h-3c-3.3 0-5 2-5 5v3H6v4h3v4h4v-4h3.2l.8-4H13V9c0-.8.2-1 1-1Z"></path></svg>
-                    </span>
-                    <span id="loginFacebookLabel" class="action-label">Đăng nhập Facebook</span>
-                  </button>
                   <button class="btn action-btn start" onclick="startJob()">
                     <span class="action-icon" aria-hidden="true">
                       <svg viewBox="0 0 24 24"><path d="M8 6.5v11l9-5.5-9-5.5Z"></path></svg>
@@ -6422,7 +6416,6 @@ function applyLanguage() {
   setText('label[for="sheet_url"]', t('sheetUrl'));
   setText('label[for="sheet_name"]', t('sheetName'));
   setText('label[for="drive_id"]', t('driveFolder'));
-  setText('#loginFacebookLabel', t('loginFacebookBeforeRun'));
   setText('#startJobLabel', t('startJob'));
   setText('#pauseJobLabel', t('stopJob'));
   setText('#continueJobLabel', t('continueJob'));
